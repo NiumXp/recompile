8b150f27e2f0c4a158ff44f63c4870c7d393bb89
NiumXp <formigacomcaimbra@gmail.com>
2022-01-02 11:35:31 -0300

    add hooks

---
+++ recompile/hooks.py
@@ -0,0 +1,54 @@
+__all__ = (
+    "hook",
+    "invoke",
+)
+
+from types import FunctionType
+from typing import (
+    Any,
+    Dict,
+    Callable,
+)
+
+from .utils import version
+
+if version(3, 10):
+    from typing import TypeAlias  # type: ignore
+else:
+    try:
+        from typing_extensions import TypeAlias
+    except (ImportError, ValueError):
+        TypeAlias = Any
+
+HookHandler = Callable[..., Any]  # type: TypeAlias
+
+_hooks = {}  # type: Dict[str, HookHandler | None]
+
+
+def hook(
+    name,  # type: str | HookHandler
+    handler=None,  # type: HookHandler | None
+):  # type: (...) -> HookHandler | None
+    """Get or register a `HookHandler` for a hook name.
+    """
+
+    if isinstance(name, FunctionType):
+        handler = name
+        name = handler.__name__
+
+    assert isinstance(name, str)
+
+    return _hooks.setdefault(name, handler)
+
+
+def invoke(
+    name,  # type: str
+    *args,  # type: Any
+    **kwargs,  # type: Any
+):  # type: (...) -> None
+    """Invoke a `HookHandler` by name.
+    """
+
+    handler = _hooks.get(name)
+    if handler:
+        handler(*args, **kwargs)
